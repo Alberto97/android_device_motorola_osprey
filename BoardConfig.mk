27da255762993a0464efa01a9e5668993bca99bd
Benjamin Legrand <android@legrand.ws>
2015-09-25 11:04:16 +0200

    [BoardConfig.mk] Remove BOARD_SEPOLICY_UNION this is not needed anymore.
    external/sepolicy/Android.mk:22: BOARD_SEPOLICY_UNION is no longer
    required - all files found in BOARD_SEPOLICY_DIRS are implicitly
    unioned; please remove from your BoardConfig.mk or other .mk file.
    
    Change-Id: I7b0952e5671f2bbffac607e12a71295d8c1aaa2a

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -122,32 +122,6 @@ include device/qcom/sepolicy/sepolicy.mk
 BOARD_SEPOLICY_DIRS += \
     $(DEVICE_PATH)/sepolicy
 
-BOARD_SEPOLICY_UNION += \
-    akmd09912.te \
-    atfwd.te \
-    atvc.te \
-    batt_health.te \
-    bluetooth_loader.te \
-    device.te \
-    file.te \
-    file_contexts \
-    healthd.te \
-    init.te \
-    init_shell.te \
-    mm-qcamerad.te \
-    property.te \
-    property_contexts \
-    qseecomd.te \
-    rild.te \
-    rmt_storage.te \
-    stml0xx.te \
-    surfaceflinger.te \
-    system_server.te \
-    tee.te \
-    ueventd.te \
-    vold.te \
-    wcnss_service.te
-
 # Vold
 TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun%d/file
 
