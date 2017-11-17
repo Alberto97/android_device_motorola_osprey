4fc5bb034b05febef8cd01dacdf8535c5249cff0
Alberto97 <albertop2197@gmail.com>
2017-11-17 14:51:59 +0100

    osprey: AOSP Bring up

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,5 @@
-# Copyright (C) 2015-2016 The CyanogenMod Project
-#           (C) 2017 The LineageOS Project
+#
+# Copyright 2015 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,17 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
 
-$(call inherit-product, device/motorola/osprey/full_osprey.mk)
-
-# Boot animation
-TARGET_SCREEN_WIDTH := 720
-TARGET_SCREEN_HEIGHT := 1280
-
-## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := osprey
-PRODUCT_NAME := lineage_osprey
-PRODUCT_MODEL := MotoG3
-PRODUCT_BRAND := Motorola
-PRODUCT_MANUFACTURER := Motorola
-PRODUCT_RELEASE_NAME := osprey
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/aosp_osprey.mk
